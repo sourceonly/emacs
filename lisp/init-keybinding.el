d1a25662d1ce19ac175fab2d23477cd29202ab19
Source <sourceonly@gmail.com>
2018-02-18 23:49:07 -0700

    1. add eshell and elisp RELP keybinding

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -1,12 +1,13 @@
 
 
-(global-set-key (kbd "C-; b") 'shell)
+(global-set-key (kbd "C-; b") 'shell)  ; common shell -> M-x shell
 (global-set-key (kbd "C-; n") '(lambda ()
 				 (interactive)
 				 (let ((buff (read-string "buffer-name: " "*shell*_")))
-				   (shell buff))))
-(global-set-key (kbd "C-; p") 'run-python)
-
+				   (shell buff))))   ; start new_shell with named buffer
+(global-set-key (kbd "C-; p") 'run-python) ; start python
+(global-set-key (kbd "C-; e") 'eshell)     ; start eshell
+(global-set-key (kbd "C-; i") 'ielm)       ; start elisp REPL
 
 					    
 (provide 'init-keybinding)
